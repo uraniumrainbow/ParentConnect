0f8c4603db9b8b5a66e150fada60281aaae7b0a3
uraniumrainbow <uraniumrainbow@gmail.com>
2023-12-04 11:47:54 -0700

    like and delete

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,5 @@
 import * as api from '../api'
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
 //action creator
 export const getPosts = () => async (dispatch) => {
@@ -20,3 +21,33 @@ export const createPost = (post) => async (dispatch) => {
         console.log(error.message);
     }
 }
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+      const { data } = await api.updatePost(id, post);
+  
+      dispatch({ type: UPDATE, payload: data });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  
+  export const likePost = (id) => async (dispatch) => {
+    try {
+      const { data } = await api.likePost(id);
+  
+      dispatch({ type: LIKE, payload: data });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  
+  export const deletePost = (id) => async (dispatch) => {
+    try {
+      await api.deletePost(id);
+  
+      dispatch({ type: DELETE, payload: id });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
