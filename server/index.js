0e0ee25ca41106b440e02457056624cb021ddf6f
NikWhit <135679785+NikWhit@users.noreply.github.com>
2023-12-03 19:00:29 -0700

    quick fixes

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://uraniumrainbow:Bowtie16@cluster0.ua1bc6p.mongodb.net/'
+const CONNECTION_URL = 'mongodb+srv://uncanickname:l6MN9bYwy4RTfSZh@nikowhit.9hcuu7n.mongodb.net/'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
