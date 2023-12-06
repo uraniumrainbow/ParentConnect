8b69e9c863f0e103a9589bbfd12bfc437dc8c8dd
uraniumrainbow <uraniumrainbow@gmail.com>
2023-12-05 17:56:43 -0700

    finale

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv'
 
 const app = express();
 
@@ -11,13 +12,17 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.get('/', (req,res) => {
+    res.send('Hello ParentConnect')
+});
 
-const CONNECTION_URL = 'mongodb+srv://uraniumrainbow:Bowtie16@cluster0.ua1bc6p.mongodb.net/'
+app.use('/posts', postRoutes);
+dotenv.config()
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
-mongoose.connect(CONNECTION_URL, { 'useFindAndModify': false })
+mongoose.connect(process.env.CONNECTION_URL, { 'useFindAndModify': false })
